f2b4f60383558396b5b53edccb76bf48b3fb6fa5
fadimanakilci <fadimekilci07@gmail.com>
2024-07-21 04:55:29 +0300

    feat: add message persistence to add file and database operations to ensure persistence by saving messages

---
+++ src/main/java/com/sparksign/persistence/MessagePersistence.java
@@ -0,0 +1,16 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright © (C) 2023 Yerlem - All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential.
+ *
+ *  Copyright © October 2023 Yerlem  @ https://yerlem.com
+ *  Written by Fadimana Kilci  <fadimekilci07@gmail.com>, July 2024
+ */
+
+package com.sparksign.persistence;
+
+public class MessagePersistence {
+    // TODO: File and database operations
+}
