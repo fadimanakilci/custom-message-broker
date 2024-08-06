0a97d8ba40ae22af8c3c277b0ca3915456acd80e
fadimanakilci <fadimekilci07@gmail.com>
2024-08-06 11:10:02 +0300

    feat: add handler service for massage

---
+++ src/main/java/com/sparksign/message/MsgHandlerService.java
@@ -0,0 +1,41 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright © February 2024 Fadimana Kilci - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Created by Fadimana Kilci  <fadimekilci07@gmail.com>, August 2024
+ */
+
+package com.sparksign.message;
+
+import com.sparksign.model.Message;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+
+public final class MsgHandlerService implements MsgHandler {
+    public MsgHandlerService() {}
+
+    @Override
+    public void handle(Object message) {
+        String _message = ((Message) message).getContent();
+        byte[] bytes                        = _message.getBytes(StandardCharsets.UTF_8);
+        ByteArrayInputStream inputStream    = new ByteArrayInputStream(bytes);
+
+        parseMessage(inputStream);
+    }
+
+    @Override
+    public void parseMessage(ByteArrayInputStream inputStream) {
+        // TODO: Parse
+    }
+
+    // Mesajları dağıtmak ve gerekli işlemleri yapmak için kullanılır
+    // message klasöründe ki message lere taşı
+    @Override
+    public void dispatchMessage(Message message) {
+        // TODO: Dispatch
+    }
+}
