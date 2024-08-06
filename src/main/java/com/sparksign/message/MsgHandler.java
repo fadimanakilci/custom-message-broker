0608b00cee0bceb3de1ff792c585370f92f382ae
fadimanakilci <fadimekilci07@gmail.com>
2024-08-06 11:09:12 +0300

    feat: add interface for message handlers

---
+++ src/main/java/com/sparksign/message/MsgHandler.java
@@ -0,0 +1,22 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright © February 2024 Fadimana Kilci - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Created by Fadimana Kilci  <fadimekilci07@gmail.com>, August 2024
+ */
+
+package com.sparksign.message;
+
+import com.sparksign.handler.Handler;
+import com.sparksign.model.Message;
+
+import java.io.ByteArrayInputStream;
+
+public interface MsgHandler extends Handler {
+    void parseMessage(ByteArrayInputStream inputStream);
+
+    void dispatchMessage(Message message);
+}
