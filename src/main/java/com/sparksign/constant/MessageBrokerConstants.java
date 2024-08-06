53f945d9db6d715708fa95c2f20926135145a6a2
fadimanakilci <fadimekilci07@gmail.com>
2024-08-06 11:10:34 +0300

    feat: add message broker constants

---
+++ src/main/java/com/sparksign/constant/MessageBrokerConstants.java
@@ -0,0 +1,29 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright © (C) 2023 Yerlem - All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential.
+ *
+ *  Copyright © October 2023 Yerlem  @ https://yerlem.com
+ *  Written by Fadimana Kilci  <fadimekilci07@gmail.com>, August 2024
+ */
+
+package com.sparksign.constant;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public final class MessageBrokerConstants {
+    public static final int           clientPort                = 1234;
+    public static final int           serverPort                = 5678;
+
+    public static final String        queueName                 = "default";
+    public static final String        clientQueueName           = "client";
+    public static final String        serverQueueName           = "server";
+    public static final String        consumerName              = "Consumer";
+
+    public static final String        clientConsumerName        = consumerName + "Client";
+    public static final String        serverConsumerName        = consumerName + "Server";
+
+    public static final AtomicInteger retryLimit                = new AtomicInteger(3);
+}
