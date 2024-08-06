b145406e5354ef7f21cd53373c9b2fbb846b6727
fadimanakilci <fadimekilci07@gmail.com>
2024-08-06 11:15:18 +0300

    style: refactor code style to make it cleaner

--- src/main/java/com/sparksign/broker/MessageQueue.java
+++ src/main/java/com/sparksign/broker/MessageQueue.java
@@ -1,12 +1,11 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * Copyright © (C) 2023 Yerlem - All Rights Reserved
- *  Unauthorized copying of this file, via any medium is strictly prohibited
- *  Proprietary and confidential.
+ * Copyright © February 2024 Fadimana Kilci - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
  *
- *  Copyright © October 2023 Yerlem  @ https://yerlem.com
- *  Written by Fadimana Kilci  <fadimekilci07@gmail.com>, July 2024
+ * Created by Fadimana Kilci  <fadimekilci07@gmail.com>, August 2024
  */
 
 package com.sparksign.broker;
@@ -20,39 +19,36 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class MessageQueue {
-    private Queue<Message> queue  = new LinkedList<>();
-    private final String name;
-    private final Object lock = new Object();
+    private final String            name;
+    private final Object            lock    = new Object();
+    private       Queue<Message>    queue   = new LinkedList<>();
 
     public MessageQueue(String name) {
-        this.name   = name;
+        this.name = name;
         loadMessages();
     }
 
     public void loadMessages() {
-        try (ObjectInputStream objectInputStream =
-                     new ObjectInputStream(new FileInputStream(name + ".queue"))) {
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(
+                new FileInputStream(name + ".queue"))) {
             queue = (Queue<Message>) objectInputStream.readObject();
         } catch (FileNotFoundException e) {
             System.out.println("No previous messages found, starting with an empty queue.");
         }  catch (IOException | ClassNotFoundException e) {
-//            queue = new LinkedList<>();
             e.printStackTrace();
         }
     }
 
     public void saveMessages() {
-        try (ObjectOutputStream objectOutputStream =
-                     new ObjectOutputStream(Files.newOutputStream(Paths.get(name + ".queue")))) {
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+                Files.newOutputStream(Paths.get(name + ".queue")))) {
             objectOutputStream.writeObject(queue);
-//            objectOutputStream.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void publish(Message message) {
-//        System.out.println("message = " + message.getContent());
         synchronized (lock) {
             queue.offer(message);
             saveMessages();
@@ -79,10 +75,7 @@ public class MessageQueue {
 
     public boolean isEmpty() {
         synchronized (lock) {
-//            System.out.println("QUEUE name = " + name + " - queue = " + queue.isEmpty());
             return queue.isEmpty();
         }
     }
-
-
 }
