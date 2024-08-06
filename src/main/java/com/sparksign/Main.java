e487087e1d26c00ee588fde096b66fd5fb32e1bf
fadimanakilci <fadimekilci07@gmail.com>
2024-08-06 11:19:28 +0300

    chore: update copyright

--- src/main/java/com/sparksign/Main.java
+++ src/main/java/com/sparksign/Main.java
@@ -1,12 +1,11 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * Copyright © (C) 2023 Yerlem - All Rights Reserved
- *  Unauthorized copying of this file, via any medium is strictly prohibited
- *  Proprietary and confidential.
+ * Copyright © February 2024 Fadimana Kilci - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
  *
- *  Copyright © October 2023 Yerlem  @ https://yerlem.com
- *  Written by Fadimana Kilci  <fadimekilci07@gmail.com>, August 2024
+ * Created by Fadimana Kilci  <fadimekilci07@gmail.com>, August 2024
  */
 
 package com.sparksign;
